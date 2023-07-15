8f284e409c0374cafb91945402632d2fda2d99af
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-07-15 11:34:23 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 /**
  * main - Entry point
  *
- *Puts 
+ * Describe the puts function 
  *
  * Return: Always 0 (Success)
  */
-int main (void){
+int main (void)
+{
       puts("\"Programming is like building a multilingual puzzle");
       return (0) ;
 }
