8531a72480d5a276059b21c9c55b03cbf82a106a
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-07-18 06:00:03 -0700

    C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet(void);
+    return (0);
+}
+ 
+ void print_alphabet ()
+ { 
+     char ch ='a' ;
+     while (ch <='z')
+     {
+     _putchar(ch);
+      ch++ ;
+     }     
+     _putchar ("\n");
+ }
