71616df22318a28b1703005d82802790f8aafae0
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-07-19 06:19:14 -0700

    C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,13 @@
 * print_alphabet_x10 - print all alphabet in lowercase 10 times
 */
 void print_alphabet_x10(void)
-{
+ {
 char letter;
-int i ;
-for(i=0;i<10;i++){ 
+int i;
+for(i = 0; i < 10; i++)
+ { 
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
-}
-}
+ }
+ }
