4d4e3c4c75fbcf903ec21e0aa4173fa38fc18966
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-07-21 05:57:38 -0700

    C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,11 @@ else if (i % 5 == 0 && i % 3 != 0 )
 printf("Buzz");
 else if (i % 3 == 0 && i % 5 == 0 )
 printf("FizzBuzz");
-else 
+else if (i == 1)
+{
+printf("%d", i);
+}
+else
 printf("%d",i);
 printf(" ");
 }
