a3dc2b98681c9330a9f560a425137a030ff64b59
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-07-21 06:56:16 -0700

    C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
  * print_diagonal, followed by a new line;
  * @n:  number of times the character \ should be printed
  */
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
 if (n <= 0)
 {
