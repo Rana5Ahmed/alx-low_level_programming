52fde86f753f6a5df55c4b706506d0793b3c1941
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-11 10:53:15 -0700

    More malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,35 +1,37 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* *_fill - fills memory with a constant byte in our case ZEROS
-* @M: memory area to be filled
-* @b: wanted value
-* @n: number of times to put b
-* Return: pointer to the memory area M
+* *_mem - fills memory with a constant byte
+* @ptr1: memory area to be filled
+* @b: char to copy
+* @n: number of times to copy b
+*
+* Return: pointer to the memory area ptr1
 */
-char *_fill(char *M, char b, unsigned int n)
+char *_mem(char *ptr1, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-M[i] = b;
+ptr1[i] = b;
 }
-return(M);
+return (ptr1);
 }
 /**
 * *_calloc - allocates memory for an array
 * @nmemb: number of elements in the array
 * @size: size of each element
-* Return: pointer ptr to allocated memory
+*
+* Return: pointer to allocated memory
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
+char *ptr;
 if (nmemb == 0 || size == 0)
-return(NULL);
-ptr = malloc(nmemb * size);
+return (NULL);
+ptr = malloc(size * nmemb);
 if (ptr == NULL)
-return(NULL);
-_fill(ptr, 0, nmemb * size);
-return(ptr);
+return (NULL);
+_mem(ptr, 0, nmemb * size);
+return (ptr);
 }
