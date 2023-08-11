f2c6427f971b7a376e87059149f2888337ed4a17
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-11 09:39:32 -0700

    More malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,17 +2,19 @@
 /**
 * *_fill - fills memory with a constant byte in our case ZEROS 
 * @M: memory area to be filled
-* @b: char to copy
-* @n: number of times to copy b
+* @b: wanted value
+* @n: number of times to put b
 *
 * Return: pointer to the memory area M
 */
 char *_fill(char *M, char b,unsigned int n )
 {
-char *zero = M;
-while(n--)
-*M++ = b;
-return(M);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+M[i] = b;
+}
+return (M);
 }
 /**
 * *_calloc - allocates memory for an array
@@ -25,10 +27,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void*ptr;
 if (nmemb == 0 || size == 0)
-return(NULL);
+return(NULL); 
 ptr = malloc(nmemb*sizeof(int)) ;
 if (ptr == NULL)
 return(NULL);
-_fill(ptr, 0, nmemb*sizeof(int));
+_fill(ptr, 0,nmemb*sizeof(int));
 return(ptr);
 }
