04a0162353be4d5b1ee6bad4ee759c54644a0be4
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-11 10:02:22 -0700

    More malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -29,9 +29,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void*ptr;
 if (nmemb == 0 || size == 0)
 return (NULL);
-ptr = malloc(nmemb*size);
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
-_fill (ptr, 0, nmemb*size);
+_fill (ptr, 0, nmemb * size);
 return (ptr);
 }
