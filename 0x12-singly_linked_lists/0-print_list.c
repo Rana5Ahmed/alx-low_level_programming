c0917ccb3983cc87557f93d19c6708303b3408cb
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-24 11:17:20 -0700

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,24 +8,14 @@
 */
 size_t print_list(const list_t *h)
 {
-    if (h == NULL)
-        return 0;
-
-    size_t count = 1;
-
-    while (1)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-
-        if (h->next == NULL)
-            break;
-
-        h = h->next;
-        count++;
-    }
-
-    return count;
+size_t count = 0;
+for (; h != NULL; h = h->next)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+count++;
+}
+return count;
 }
