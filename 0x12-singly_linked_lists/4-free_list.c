52e91f6d8531ce6f022dc572a2b72b182023bb49
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-24 12:07:21 -0700

    linked

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,20 +3,15 @@
 /**
 * free_list - frees a linked list
 * @head: list_t list to be freed
-*
-* Return: void
 */
 void free_list(list_t *head)
 {
-list_t *node,*next_node;
-if (!head)
-return;
-node = head;
-while (node)
+list_t *temp;
+while (head)
 {
-next_node = node->next;
-free (node->str);
-free (node);
-node = next_node;
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
 }
 }
