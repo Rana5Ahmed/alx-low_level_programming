69c350a46f8b9f1ef533f10f0911913b8a6b8712
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-24 11:06:51 -0700

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,12 @@
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
-    
     for (; h != NULL; h = h->next)
     {
         if (h->str == NULL)
             printf("[0] (nil)\n");
         else
             printf("[%u] %s\n", h->len, h->str);
-        
         count++;
     }
     return count;
